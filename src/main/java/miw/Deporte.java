19cd65416aef4875d4d40460facbd6fdda4b742f
cpalacios <c.palaciosc@alumnos.upm.es>
2014-10-01 17:09:01 +0200

    CR1.01

--- src/main/java/miw/Deporte.java
+++ src/main/java/miw/Deporte.java
@@ -16,5 +16,47 @@ public class Deporte {
 		this.id = id;
 		this.nombre = nombre;
 		this.descripion = descripion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripion() {
+		return descripion;
+	}
+
+	public void setDescripion(String descripion) {
+		this.descripion = descripion;
+	}
+
+	public int getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+
+	public int getNumeroJugadores() {
+		return numeroJugadores;
+	}
+
+	public void setNumeroJugadores(int numeroJugadores) {
+		this.numeroJugadores = numeroJugadores;
 	}	
+	
+	
 }
